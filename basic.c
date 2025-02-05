c8a71ffe37d3d6d43d60855f6f4edccb183e66dd
qosimovkomiljon7 <qosimovkomiljon7@gmail.com>
2025-02-05 09:25:32 +0000

    some changes

--- basic.c
+++ basic.c
@@ -1,8 +1,75 @@
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:2005040696.
+// Suggested code may be subject to a license. Learn more: ~LicenseLog:2701344185.
 #include <stdio.h>
 #include <stdlib.h>
 
-int main () {
-    int random;
-    scanf('%d', random);
-    printf("you input %d", random);
+int main(){
+    for (int i = 0; i < 100; i++) {
+        printf("%d\n", i + 1);
+    }
+    return 0;
+}
+
+int flag () {
+    //create a simple program to print shapes with *
+    int rows, i, j;
+
+    // Get the number of rows from the user
+    printf("Enter the number of rows: ");
+    scanf("%d", &rows);
+
+    // Print a right-angled triangle
+    printf("Right-angled Triangle:\n");
+    for (i = 1; i <= rows; i++) {
+        for (j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    // Print an inverted right-angled triangle
+    printf("\nInverted Right-angled Triangle:\n");
+    for (i = rows; i >= 1; i--) {
+        for (j = 1; j <= i; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+
+    // Print a pyramid
+
+    printf("\nPyramid:\n");
+    for (i = 1; i <= rows; i++) {
+        for (j = 1; j <= rows - i; j++) {
+            printf(" ");
+        }
+        for (j = 1; j <= 2 * i - 1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+     // Print a diamond
+    printf("\nDiamond:\n");
+    for (i = 1; i <= rows; i++) {
+        for (j = 1; j <= rows - i; j++) {
+            printf(" ");
+        }
+        for (j = 1; j <= 2 * i - 1; j++) {
+            printf("*");
+        }
+        printf("\n");
+    }
+    for (i = rows - 1; i >= 1; i--) {
+        for (j = 1; j <= rows - i; j++) {
+            printf(" ");
+        }
+        for (j = 1; j <= 2 * i - 1; j++) {
+            printf
+("*");
+        }
+        printf("\n");
+    }
+
+
+    return 0;
 }
